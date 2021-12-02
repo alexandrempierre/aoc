96e44254046c37a3ea28064a6bbf0f1a8f4570ef
Alexandre Pierre <alexandrempierre@gmail.com>
2021-12-02 13:24:19 -0300

    Day 02 Part Two

--- day02/main.py
+++ day02/main.py
@@ -7,12 +7,16 @@
 if __name__ == '__main__':
     with open('input', 'r', encoding='ascii') as fp:
         depth, horizontal = 0, 0
+        aim = 0
         for line in fp:
             steps = int(line.rpartition(' ')[2])
             if line.startswith('forward'):
                 horizontal += steps
+                depth += aim * steps
             elif line.startswith('up'):
                 depth -= steps
+                aim -= steps
             elif line.startswith('down'):
                 depth += steps
+                aim += steps
         print(horizontal * depth)
