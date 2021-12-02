2440c6a4a6ff261e271ce3441845660d0bddc65b
Alexandre Pierre <alexandrempierre@gmail.com>
2021-12-02 13:03:33 -0300

    Day 01 Part 2

--- day01/main.py
+++ day01/main.py
@@ -12,6 +12,19 @@ def pair(generator):
         prev = curr
 
 
+def triple(generator):
+    ''' '''
+    val1 = next(generator)
+    val2 = next(generator)
+    for val3 in generator:
+        yield val1, val2, val3
+        val1, val2 = val2, val3
+
+
 if __name__ == '__main__':
     with open('./input', 'r', encoding='ascii') as fp:
+        print('Part One: ', end='')
         print(sum(n > m for m, n in pair(map(int, fp))))
+        fp.seek(0, 0)
+        print('Part Two: ', end='')
+        print(sum(n > m for m, n in pair(map(sum, triple(map(int, fp))))))
